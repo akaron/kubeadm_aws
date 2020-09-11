0c80fb50b8dbda1c9fd332c6944a1b5362c9b201
KaiLung Sun <akaronsun@gmail.com>
2020-09-11 16:06:53 +0800

    dynamic template files

--- terraform/local_file.tf
+++ terraform/local_file.tf
@@ -1,10 +1,8 @@
 resource "local_file" "inventory" {
   # depends_on = [aws_autoscaling_group.controlplane, aws_autoscaling_group.worker]
   content = templatefile("inventory.tmpl", {
-    cp1 = data.aws_instances.controlplane.public_ips[0]
-    worker1 = data.aws_instances.worker.public_ips[0]
-    # worker2 = data.aws_instances.worker.public_ips[1]
-    # worker3 = data.aws_instances.worker.public_ips[2]
+    controlplane_ips = zipmap(range(length(data.aws_instances.controlplane.public_ips)), data.aws_instances.controlplane.public_ips)
+    worker_ips = zipmap(range(length(data.aws_instances.worker.public_ips)), data.aws_instances.worker.public_ips)
     ssh_key_file = var.ssh_private_key_file_name
     cluster_name = var.cluster_name
     interpreter_python = var.interpreter_python
