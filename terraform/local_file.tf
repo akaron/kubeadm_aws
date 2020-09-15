87468af0e732f31b8a6bfd8ae22479586b8aee3c
KaiLung Sun <akaronsun@gmail.com>
2020-09-15 22:12:45 +0800

    bug fix: generate correct inventory file

--- terraform/local_file.tf
+++ terraform/local_file.tf
@@ -1,8 +1,8 @@
 resource "local_file" "inventory" {
   # depends_on = [aws_autoscaling_group.controlplane, aws_autoscaling_group.worker]
   content = templatefile("inventory.tmpl", {
-    controlplane_ips = zipmap(range(length(data.aws_instances.controlplane.public_ips)), data.aws_instances.controlplane.public_ips)
-    worker_ips = zipmap(range(length(data.aws_instances.worker.public_ips)), data.aws_instances.worker.public_ips)
+    controlplane_ips = zipmap(range(1, 1+length(data.aws_instances.controlplane.public_ips)), data.aws_instances.controlplane.public_ips)
+    worker_ips = zipmap(range(1, 1+length(data.aws_instances.worker.public_ips)), data.aws_instances.worker.public_ips)
     ssh_key_file = var.ssh_private_key_file_name
     cluster_name = var.cluster_name
     interpreter_python = var.interpreter_python
