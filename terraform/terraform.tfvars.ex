253e1e395608f542b6963cd28cdc0b6a836c0dc0
K.-L Sun <akaronsun@gmail.com>
2021-01-22 22:21:05 +0800

    updates

--- terraform/terraform.tfvars.ex
+++ terraform/terraform.tfvars.ex
@@ -1,19 +1,33 @@
-#
-# Default values, modify if you want and you know what you're doing
-#
-# aws_profile = "default"
-# prefix = "quickstart"  # prefix for name or tag of some aws resources
-# instance_type_controlplane = "t3.medium"  # at least "t3.small" or "t3a.small"
-# instance_type_worker = "t3a.medium"
-# volume_size_controlplane = "12"  # at least 9 GB 
-# volume_size_worker = "13"
-# ssh_private_key_file_name = "/vagrant/mykey"  # the full path
-# ssh_public_key_file_name = "/vagrant/mykey.pub"
+# Please fill in all values
+# The comment lines are optional variables with their default values filled
 
-#
-# Please fill in all values below
-#
+# aws_profile = "default"
 aws_region = ""  # such as "us-east-2"
-myip_cidr = ""  # basically it's your external ip, such as "1.2.3.4/32" (try `curl ifconfig.me` to find it out)
+
+# myip_cidr: for aws security group to open some ports to this machine
+# should use something other than "0.0.0.0/32"
+myip_cidr = ""
 route53_hosted_zone = ""  # assume the hosted_zone already created. such as "example.com." (there's a dot in the end)
 cluster_name = ""  # such as "example.com"
+
+interpreter_python = "/usr/bin/python3"  # for ansible inventory file
+
+# Prefix for most resources
+# prefix = "quickstart"
+
+# EC2 instance
+# instance_type_controlplane = "t3a.small"  # for test purpose
+# instance_type_worker = "t3a.small"
+ssh_private_key_file_name = ""  # the full path
+ssh_public_key_file_name = ""
+# volume_size_controlplane = "10"  # at least 9 GB 
+# volume_size_worker = "10"
+
+# notes:
+# * recommended instance type and EBS volume sizes for simple tests:
+#   - 2 vCPU and 2 GB RAM (t3.small or t3a.small), 8GB of EBS
+#   - notes
+#     - in a test run using t3a.small, there are ~1.2-1.4GB of free RAM in worker nodes with
+#       prometheus, grafana, and cert-manager running, and 3-4 GB of EBS has been used
+#     - t3.micro/small/medium: all 2 vCPU and 1/2/4 GB of ram, daily costs are about
+#       0.25/0.5/1.0 US dollar per instance
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -25,37 +25,35 @@ variable "prefix" {
 variable "ssh_private_key_file_name" {
   type        = string
   description = "File path and name of SSH private key used for infrastructure"
-  default     = "/vagrant/mykey"
 }
 
 variable "ssh_public_key_file_name" {
   type        = string
   description = "File path and name of SSH public key used for infrastructure"
-  default     = "/vagrant/mykey.pub"
 }
 
 variable "instance_type_controlplane" {
   type        = string
   description = "EC2 Instance type used for controlplane nodes"
-  default     = "t3.medium"
+  default     = "t3a.medium"
 }
 
 variable "instance_type_worker" {
   type        = string
   description = "EC2 Instance type used for worker nodes"
-  default     = "t3.medium"
+  default     = "t3a.medium"
 }
 
 variable "volume_size_controlplane" {
   type        = string
   description = "root volume size used for all controlplane nodes; at least 8GB"
-  default     = "12"  # GiB
+  default     = "10"  # GiB
 }
 
 variable "volume_size_worker" {
   type        = string
   description = "root volume size used for all worker nodes; at least 8GB"
-  default     = "13"  # GiB
+  default     = "10"  # GiB
 }
 
 variable "route53_hosted_zone" {
@@ -71,8 +69,7 @@ variable "myip_cidr" {
 
 variable "interpreter_python" {
   type        = string
-  description = "interpreter of python for ansible (in local)"
-  default     = "/home/vagrant/venv/bin/python3"
+  description = "interpreter of python for ansible"
 }
 
 
